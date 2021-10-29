0f63b508a25a50aa1a4d0cc54c515c4bbb0f71a1
vic-meateater <victor.myasoedov@gmail.com>
2021-10-29 16:42:57 +0500

    Start Fire Effect

--- Assets/Code/TankUnit/Code/TankView.cs
+++ Assets/Code/TankUnit/Code/TankView.cs
@@ -2,16 +2,33 @@
 using Markers;
 using TankUnit.Interfaces;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace TankUnit.Code
 {
     public class TankView : TankClass, ITankView, ITakeDamage
     {
+        private Object _shellExplosionPrefab;
+        private Transform _shellExplosionPosition;
         public bool ai { get; set; }
+
+        private void Awake()
+        {
+            _shellExplosionPrefab = Resources.Load("Prefabs/ShellExplosion");
+            _shellExplosionPosition = gameObject.GetComponent<BulletMarker>().transform;
+        }
+
         public float TakeDamage(float damage)
         {
+            
             float _damage = damage;
             if (!ai) TankFire.Fire();
+            //if (!gameObject.TryGetComponent(out _shellExplosionPrefab))
+            //{
+            //    Debug.Log("TryGetComponent");
+            //}
+            Instantiate(_shellExplosionPrefab, _shellExplosionPosition);
+            //Destroy();
             return damage;
         }
     }
