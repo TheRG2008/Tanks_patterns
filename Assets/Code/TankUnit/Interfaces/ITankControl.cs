62a1471ce81616a0859f8713a257cdd11ee866b1
blindfolded32 <sunsya@yandex.ru>
2021-11-01 21:40:34 +0300

    Random targets

--- Assets/Code/TankUnit/Interfaces/ITankControl.cs
+++ Assets/Code/TankUnit/Interfaces/ITankControl.cs
@@ -1,7 +1,10 @@
-﻿namespace TankUnit.Interfaces
+﻿using UnityEngine;
+
+namespace TankUnit.Interfaces
 {
     public interface ITankControl
     {
+        void RotateTo(Transform transform);
         void NewTurn();
     }
 }
